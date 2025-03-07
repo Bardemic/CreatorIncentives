bc206381f48c5a799c17f4a85989c1094e3ccccd
Bardemic <134565367+Bardemic@users.noreply.github.com>
2025-03-07 01:03:24 -0600

    Add Frontend & gitignore

---
+++ frontend/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div>
+        CreatorIncentives
+      </div>
+    </div>
+  );
+}
